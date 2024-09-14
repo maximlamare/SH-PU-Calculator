5660ca1c527704db8f2e07d8815bbe4c2db9a1a4
Maxim Lamare <maxim.lamare@sinergise.com>
2024-09-14 13:25:22 +0200

    Update file reader

--- pucalc/src/GeoJSONAreaCalculator.js
+++ pucalc/src/GeoJSONAreaCalculator.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import * as turf from '@turf/turf';
-import MapComponent from './MapComponent';
+import Tooltip from './components/Tooltip/Tooltip';
 
-const GeoJSONAreaCalculator = () => {
+
+const GeoJSONAreaCalculator = ({ setGeoJSONData, clearGeoJSONData }) => {
     const [file, setFile] = useState(null);
-    const [area, setArea] = useState(null);
     const [error, setError] = useState(null);
-    const [geoJSONData, setGeoJSONData] = useState(null);
+    const fileInputRef = useRef(null);
 
     const handleFileChange = (event) => {
         setFile(event.target.files[0]);
+    };
+
+    const handleClear = () => {
+        // Clear functions for the clear button
+        setError(null);
+        setFile(null);
+        if (fileInputRef.current) {
+            fileInputRef.current.value = '';
+        }
+        clearGeoJSONData();
     };
 
     const handleCalculateArea = () => {
@@ -22,50 +32,73 @@ const GeoJSONAreaCalculator = () => {
         reader.onload = (e) => {
             try {
                 const geoJSON = JSON.parse(e.target.result);
-                let area = 0;
-
-                if (geoJSON.type === 'FeatureCollection' && geoJSON.features) {
-                    area = turf.area(geoJSON);
-                } else if (geoJSON.type === 'Feature' && geoJSON.geometry) {
-                    area = turf.area(geoJSON);
-                } else if (geoJSON.type === 'GeometryCollection' && geoJSON.geometries) {
-                    geoJSON.geometries.forEach(geometry => {
-                        area += turf.area(geometry);
-                    });
-                } else if (geoJSON.type === 'Polygon' || geoJSON.type === 'MultiPolygon') {
-                    area = turf.area(geoJSON);
-                } else {
-                    throw new Error('Invalid GeoJSON file: Unsupported type or missing properties.');
-                }
-
-                setArea(area);
                 setGeoJSONData(geoJSON);
                 setError(null);
             } catch (error) {
                 console.error('Error reading GeoJSON file:', error);
                 setError('Error reading GeoJSON file: ' + error.message);
-                setArea(null);
                 setGeoJSONData(null);
             }
         };
         reader.onerror = (e) => {
             console.error('Error reading file:', e);
             setError('Error reading file: ' + e.target.error.message);
-            setArea(null);
             setGeoJSONData(null);
         };
+
         reader.readAsText(file);
     };
 
     return (
-        <div>
-            <h1>GeoJSON Area Calculator</h1>
-            <input type="file" accept=".geojson" onChange={handleFileChange} />
-            <button onClick={handleCalculateArea}>Calculate Area</button>
+
+        <div className="bg-gray-100 rounded-lg min-h overflow-y-auto">
+            <h2 className="heading-secondary">
+                <div className="flex items-center">
+                    <div className="mr-2">
+                        GeoJSON file:
+                    </div>
+                    <Tooltip
+                        infoStyles="ml-1"
+                        content="Upload a GeoJSON file containing a polygon or multi-polygons of your AOI. Alternatively, you can draw a polygon on the map."
+                        direction="right"
+                        wrapperClassName="tooltip-content"
+                    />
+                </div>
+            </h2>
+            <br />
+            <div className="flex items-center space-x-4">
+                <input type="file"
+                    accept=".geojson"
+                    onChange={handleFileChange}
+                    ref={fileInputRef}
+                    className="flex-grow max-w-full "
+                    style={{
+                        overflow: 'hidden',
+                        whiteSpace: 'nowrap',
+                        textOverflow: 'ellipsis',
+                    }} />
+                <button
+                    onClick={handleCalculateArea}
+                    className="flex justify-right items-center secondary-button ml-auto"
+                >
+                    Calculate Area
+                </button>
+                <button
+                    onClick={handleClear}
+                    className="flex justify-right items-center tertiary-button ml-2"
+                >
+                    Clear
+                </button>
+            </div>
             {error && <p style={{ color: 'red' }}>{error}</p>}
-            {area !== null && <p>Area: {area} square meters</p>}
-            <MapComponent geoJSONData={geoJSONData} />
+            <br />
+
+
+
         </div>
+
+
+
     );
 };
 
