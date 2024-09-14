a90e18cba5bc656f08f65af843933e34c7d6b8c0
Maxim Lamare <maxim.lamare@sinergise.com>
2024-09-14 13:26:07 +0200

    Add header component

---
+++ pucalc/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const HeaderLogo = () => {
+    return (
+        <>
+            <img
+                src={'/sentinel_hub_logo_big.png'}
+                className="sm:w-48 w-24 h-auto"
+                alt="logo"
+            />
+            <h1 className="heading-primary ml-1">
+                <i className="text-primary cursor-default">Basic PU Calculator</i>
+            </h1>
+        </>
+    );
+};
+
+export default HeaderLogo;
