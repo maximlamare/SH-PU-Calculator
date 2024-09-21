d024af2d232ae37fb460316a3ab6e314afc52fac
Maxim Lamare <maxim.lamare@pm.me>
2024-09-21 14:36:55 +0200

    Updates to App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import HeaderLogo from './components/Header.js';
-import GeoJSONAreaCalculator from './GeoJSONAreaCalculator.js';
-import MapComponent from './MapComponent.js';
-import AreaBox from './AreaBox.js';
+import GeoJSONAreaCalculator from './components/Geo/GeoJSONAreaCalculator.js';
+import MapComponent from './components/Geo/MapComponent.js';
+import AreaBox from './components/Geo/AreaBox.js';
+import Parameters from './components/Parameters.js';
+import PUBox from './components/Results.js';
 
 const App = () => {
   const [geoJSONData, setGeoJSONData] = useState(null);
-  const [area, setArea] = useState(null); // Add state for area
+  const [area, setArea] = useState(0);
+  const [PUs, setPUs] = useState(0);
+  const [computeClicked, setComputeClicked] = useState(false);
+  const [reset, setReset] = useState(false);
   const clearGeoJSONRef = useRef(null);
 
+  const handleAreaUpdate = useCallback((newArea) => {
+    console.log('handleAreaUpdate called with:', newArea);
+
+    setArea(newArea);
+  }, []);
+
+  const handleComputePUs = useCallback((result) => {
+    setPUs(result);
+    setComputeClicked(true);
+    setReset(false);
+  }, []);
+
+  const handleReset = useCallback(() => {
+    setPUs(null);
+    setComputeClicked(false);
+    setReset(true);
+  }, []);
 
   const clearGeoJSONData = () => {
-    // Clear the map when the clear button is hit
     setGeoJSONData(null);
-    setArea(null);
     if (clearGeoJSONRef.current) {
       clearGeoJSONRef.current();
     }
@@ -22,7 +42,7 @@ const App = () => {
   return (
     <div className="App">
       <div id="code-editor-modal"></div>
-      <div className="flex items-center justify-between lg:flex-row flex-col">
+      <div className="flex items-center justify-between lg:flex-row flex-co mx-4 lg:mx-4">
         <div className="flex items-center">
           <HeaderLogo />
         </div>
@@ -32,12 +52,22 @@ const App = () => {
           <GeoJSONAreaCalculator setGeoJSONData={setGeoJSONData} clearGeoJSONData={clearGeoJSONData} />
           <div className="my-3"></div>
           <AreaBox area={area} />
+          <div className="my-3"></div>
+          <Parameters
+            area={area}
+            onComputePUs={handleComputePUs}
+            onReset={handleReset}
+          />
+          <div className="my-3"></div>
+          <PUBox PUs={PUs} computeClicked={computeClicked} reset={reset} />
         </div>
-        <div className="w-1/2 p-1" style={{ height: '90vh' }}>
-          <MapComponent geoJSONData={geoJSONData} setArea={setArea} clearGeoJSON={clearGeoJSONRef} />
+        <div className="w-1/2 p-1 h-64">
+          <MapComponent
+            onAreaUpdate={handleAreaUpdate}
+          />
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
