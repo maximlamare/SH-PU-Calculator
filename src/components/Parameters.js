d9f370ffcdbadcc2ad4a3a87ec69e46223c15c6b
Maxim Lamare <maxim.lamare@pm.me>
2024-10-17 18:39:49 +0200

    Add parameters to account for inputs and classic resolution products

--- src/components/Parameters.js
+++ src/components/Parameters.js
@@ -3,15 +3,36 @@ import { totalPuContribution } from '../js/functions/puContributors';
 import { calculateAutoDimensions } from '../js/functions/geoCalculations';
 
 const Parameters = ({ geoJSONData, onComputePUs, onReset }) => {
+    const [mosaicking, setMosaicking] = useState('SIMPLE'); // Add state for mosaicking
     const [resolution, setResolution] = useState(10);
+    const [resolutionClass, setResolutionClass] = useState(1);
     const [inputBands, setInputBands] = useState(3);
     const [dataType, setDataType] = useState('8bit');
-    const [outputRasters, setOutputRasters] = useState(1);
+    const [inputSamples, setInputSamples] = useState(1); // Add state for the extra number input
 
-    const handleResolutionChange = (e) => setResolution(e.target.value);
+
+    const handleMosaickingChange = (e) => { setMosaicking(e.target.value); };
     const handleInputBandsChange = (e) => setInputBands(e.target.value);
     const handleDataTypeChange = (e) => setDataType(e.target.value);
-    const handleOutputRastersChange = (e) => setOutputRasters(e.target.value);
+    const handleInputSamplesChange = (e) => { setInputSamples(e.target.value); };
+
+    const resolutionMapping = {
+        'S210': 10,
+        'L8': 30,
+        'PS': 3,
+        'SkySat': 0.5,
+        'OTHER': 1
+    };
+    const handleResolutionClassChange = (e) => {
+        const value = e.target.value;
+        setResolutionClass(value);
+        setResolution(resolutionMapping[value]);
+    };
+
+    const handleResolutionChange = (e) => {
+        const value = e.target.value;
+        setResolution(value);
+    };
 
     const handleComputePUs = () => {
         if (!geoJSONData) {
@@ -21,22 +42,23 @@ const Parameters = ({ geoJSONData, onComputePUs, onReset }) => {
 
         // Get height and width from geoJSONData
         const [height, width] = calculateAutoDimensions(geoJSONData, resolution);
-
         const result = totalPuContribution(
             height,
             width,
             inputBands,
             dataType,
-            outputRasters
+            inputSamples
         );
         onComputePUs(result);
     };
 
     const handleReset = () => {
+        setMosaicking('SIMPLE');
+        setResolutionClass('S210');
+        setInputSamples(1);
         setResolution(10);
         setInputBands(3);
         setDataType('8bit');
-        setOutputRasters(1);
         onComputePUs(null);
         onReset(true);
     };
@@ -47,15 +69,66 @@ const Parameters = ({ geoJSONData, onComputePUs, onReset }) => {
                 <h2 className="text-xl font-bold mb-4">Request Parameters</h2>
                 <div className="mb-4">
                     <label className="block mb-2">
-                        Resolution (m):
-                        <input
-                            type="number"
-                            value={resolution}
-                            onChange={handleResolutionChange}
+                        <a href="https://docs.sentinel-hub.com/api/latest/evalscript/v3/#mosaicking"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-shgreen">Mosaicking</a> method:
+                        <select
+                            value={mosaicking}
+                            onChange={handleMosaickingChange}
                             className="ml-2 p-1 border rounded"
-                        />
+                        >
+                            <option value="SIMPLE">SIMPLE</option>
+                            <option value="ORBIT">ORBIT</option>
+                            <option value="TILE">TILE</option>
+                        </select>
                     </label>
                 </div>
+                {mosaicking === 'ORBIT' || mosaicking === 'TILE' ? (
+                    <div className="mb-4">
+                        <label className="block mb-2">
+                            Number of <a href="https://docs.sentinel-hub.com/api/latest/evalscript/v3/#samples"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-shgreen">input samples</a> (images):
+                            <input
+                                type="number"
+                                value={inputSamples}
+                                onChange={handleInputSamplesChange}
+                                className="ml-2 p-1 border rounded"
+                            />
+                        </label>
+                    </div>
+                ) : null}
+                <div className="mb-4">
+                    <label className="block mb-2">
+                        Resolution (m):
+                        <select
+                            value={resolutionClass}
+                            onChange={handleResolutionClassChange}
+                            className="ml-2 p-1 border rounded"
+                        >
+                            <option value="S210">Sentinel-2: 10m</option>
+                            <option value="L8">Landsat: 30m</option>
+                            <option value="PS">PlanetScope: 3m</option>
+                            <option value="SkySat">SkySat: 0.5m</option>
+                            <option value="OTHER">Other</option>
+                        </select>
+                    </label>
+                </div>
+                {resolutionClass === 'OTHER' ? (
+                    <div className="mb-4">
+                        <label className="block mb-2">
+                            Custom Resolution (m):
+                            <input
+                                type="number"
+                                value={resolution}
+                                onChange={handleResolutionChange}
+                                className="ml-2 p-1 border rounded"
+                            />
+                        </label>
+                    </div>
+                ) : null}
                 <div className="mb-4">
                     <label className="block mb-2">
                         Number of <a href="https://docs.sentinel-hub.com/api/latest/evalscript/v3/#input-object-properties"
@@ -85,17 +158,6 @@ const Parameters = ({ geoJSONData, onComputePUs, onReset }) => {
                             <option value="16bit">16bit</option>
                             <option value="32bit">32bit</option>
                         </select>
-                    </label>
-                </div>
-                <div className="mb-4">
-                    <label className="block mb-2">
-                        Number of output rasters from the Request:
-                        <input
-                            type="number"
-                            value={outputRasters}
-                            onChange={handleOutputRastersChange}
-                            className="ml-2 p-1 border rounded"
-                        />
                     </label>
                 </div>
                 <div className="flex justify-center w-full gap-4">
