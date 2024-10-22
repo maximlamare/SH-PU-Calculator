8d0714bbb1e71e0c5d65e51049c47a94d089a870
Maxim Lamare <maxim.lamare@pm.me>
2024-10-22 18:04:38 +0200

    Update handling of polygons

--- src/components/Geo/MapComponent.js
+++ src/components/Geo/MapComponent.js
@@ -8,76 +8,98 @@ import L from 'leaflet';
 import { computeArea } from '../../js/functions/geoCalculations';
 
 
-const MapComponent = ({ onAreaUpdate, geoJSONData, clearGeoJSONRef }) => {
+const MapComponent = ({ onAreaUpdate, geoJSONData, clearGeoJSONRef, onGeoJSONUpdate }) => {
   const position = [47.06, 15.44]; // Graz coordinates
   const [polygons, setPolygons] = useState([]);
   const featureGroupRef = useRef();
   const mapRef = useRef();
 
+
   const updateTotalArea = useCallback((newPolygons) => {
-    const newTotalArea = newPolygons.reduce((sum, polygon) => sum + polygon.area, 0);
+    const newTotalArea = newPolygons.reduce((sum, polygon) => sum + computeArea(polygon), 0);
     onAreaUpdate(newTotalArea);
   }, [onAreaUpdate]);
 
-  const onCreated = useCallback((e) => {
-    const { layer } = e;
-    const geoJSON = layer.toGeoJSON();
-    const area = computeArea(geoJSON);
+  const updateGeoJSON = useCallback((newPolygons) => {
+    const mergedGeoJSON = {
+      type: "FeatureCollection",
+      features: newPolygons
+    };
+    onGeoJSONUpdate(mergedGeoJSON);
 
-    setPolygons(prevPolygons => {
-      const newPolygons = [...prevPolygons, { layer, area }];
-      updateTotalArea(newPolygons);
-      return newPolygons;
-    });
-  }, [updateTotalArea]);
-
-  const onDeleted = useCallback((e) => {
-    const { layers } = e;
-    setPolygons(prevPolygons => {
-      const newPolygons = prevPolygons.filter(polygon => !layers.hasLayer(polygon.layer));
-      updateTotalArea(newPolygons);
-      return newPolygons;
-    });
-  }, [updateTotalArea]);
+  }, [onGeoJSONUpdate]);
 
   const clearAllPolygons = useCallback(() => {
     if (featureGroupRef.current) {
       featureGroupRef.current.clearLayers();
     }
     updateTotalArea([]);
-  }, [updateTotalArea]);
+    updateGeoJSON([]);
+  }, [updateTotalArea, updateGeoJSON]);
+
+  const onCreated = useCallback((e) => {
+    const { layer } = e;
+
+    const geoJSON = layer.toGeoJSON();
+
+    setPolygons(prevPolygons => {
+      const newPolygons = [...prevPolygons, geoJSON];
+      updateTotalArea(newPolygons);
+      updateGeoJSON(newPolygons);
+      return newPolygons;
+    });
+  }, [updateTotalArea, updateGeoJSON]);
+
+  const onDeleted = useCallback((e) => {
+
+    // // Assuming 'featureGroupRef' is your Leaflet FeatureGroup
+    const allPolygons = featureGroupRef.current.getLayers()
+    const geoPolys = allPolygons.map(polygon => polygon.toGeoJSON())
+    let allFeatures = [];
+
+    geoPolys.forEach(geoPoly => {
+      if (geoPoly.type === "FeatureCollection") {
+        allFeatures = allFeatures.concat(geoPoly.features);
+        console.log(allFeatures)
+      } else if (geoPoly.type === "Feature") {
+        allFeatures.push(geoPoly);
+      }
+    });
+
+    setPolygons(allFeatures);
+    updateTotalArea(allFeatures);
+    updateGeoJSON(allFeatures);
+
+  }, [setPolygons, updateTotalArea, updateGeoJSON]);
+
+
+  useEffect(() => {
+    if (geoJSONData) {
+      const currentFeatureGroup = featureGroupRef.current;
+      currentFeatureGroup.clearLayers();
+
+      const geoJSONLayer = L.geoJSON(geoJSONData).addTo(currentFeatureGroup);
+
+      const bounds = geoJSONLayer.getBounds();
+      mapRef.current.fitBounds(bounds);
+
+      const geoJSONpolygons = geoJSONData.features.filter(feature => {
+        return feature.geometry.type === 'Polygon' || feature.geometry.type === 'MultiPolygon';
+      });
+
+      setPolygons(geoJSONpolygons)
+      updateTotalArea(geoJSONpolygons);
+      updateGeoJSON(geoJSONpolygons);
+
+    }
+  }, [geoJSONData, setPolygons, updateTotalArea, updateGeoJSON]);
 
   useEffect(() => {
     clearGeoJSONRef.current = clearAllPolygons;
   }, [clearAllPolygons, clearGeoJSONRef]);
 
-  useEffect(() => {
-    if (geoJSONData) {
-      // Add to map after clearing
-      const currentFeatureGroup = featureGroupRef.current;
-      currentFeatureGroup.clearLayers();
-      const geoJSONLayer = L.geoJSON(geoJSONData).addTo(currentFeatureGroup);
-      // Assign a unique ID to the layer
-      const layerId = L.stamp(geoJSONLayer);
-      const bounds = geoJSONLayer.getBounds();
-      mapRef.current.fitBounds(bounds);
-
-      // Calculate the are
-      const area = computeArea(geoJSONData);
-
-      setPolygons(prevPolygons => {
-        const newPolygons = [...prevPolygons, { layer: geoJSONLayer, area, id: layerId }];
-        updateTotalArea(newPolygons);
-        return newPolygons;
-      });
-
-      // Update the total area
-      updateTotalArea([{ area }]);
-    }
-  }, [geoJSONData, updateTotalArea]);
-
   return (
-    <MapContainer ref={mapRef} center={position} zoom={13} style={{ height: "400px", width: "100%" }}>
+    <MapContainer ref={mapRef} center={position} zoom={13} style={{ height: "100%", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
