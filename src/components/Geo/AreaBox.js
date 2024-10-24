5ff39c42e07fc2918f1ba8d56634711cac497847
Maxim Lamare <maxim.lamare@pm.me>
2024-10-24 18:17:15 +0200

    Padding of Area

--- src/components/Geo/AreaBox.js
+++ src/components/Geo/AreaBox.js
@@ -6,7 +6,7 @@ const AreaBox = ({ area }) => {
         <div className="flex bg-gray-100 rounded-lg min-h overflow-y-auto items-center">
             <div style={{ display: 'flex', alignItems: 'center' }}>
 
-                <p className="text mx-1 my-3 h-full flex items-center">
+                <p className="text mx-1 pl-4 my-3 h-full flex items-center">
                     <span className="mr-1">Area of AOI:</span>
                 </p>
                 {area !== null && <p>{(area * 1e-6).toFixed(2)} km<sup>2</sup> </p>}
