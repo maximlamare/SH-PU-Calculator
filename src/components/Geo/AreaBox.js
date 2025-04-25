36c640208f49f8e8ace6345da3c7537a681f6f23
Blaž Pavlica <blaz.pavlica@gmail.com>
2025-04-25 16:49:38 +0200

    formatting

--- src/components/Geo/AreaBox.js
+++ src/components/Geo/AreaBox.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const AreaBox = ({ area }) => {
-
-    return (
-        <div className="flex bg-gray-100 rounded-lg min-h overflow-y-auto items-center">
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-
-                <p className="text mx-1 pl-4 my-3 h-full flex items-center">
-                    <span className="mr-1">Area of AOI:</span>
-                </p>
-                {area !== null && <p>{(area * 1e-6).toFixed(2)} km<sup>2</sup> </p>}
-            </div>
-        </div>
-    );
+	return (
+		<div className="flex bg-gray-100 rounded-lg min-h overflow-y-auto items-center">
+			<div style={{ display: "flex", alignItems: "center" }}>
+				<p className="text mx-1 pl-4 my-3 h-full flex items-center">
+					<span className="mr-1">Area of AOI:</span>
+				</p>
+				{area !== null && (
+					<p>
+						{(area * 1e-6).toFixed(2)} km<sup>2</sup>{" "}
+					</p>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default AreaBox;
